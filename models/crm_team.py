3931e052b36cbb8d70ac810c7f1b9a3fe3b5eec7
Muhammed Anas <muhammadanaswayanad@gmail.com>
2025-04-21 11:53:21 +0530

    Add member_company_ids field and update CRM Team Lead form view

--- models/crm_team.py
+++ models/crm_team.py
@@ -3,6 +3,14 @@ from odoo import models, fields, api
 class CrmTeamLead(models.Model):
     _inherit = 'crm.team'
 
+    member_company_ids = fields.Many2many('res.company', string='Member Companies', 
+                                        compute='_compute_member_company_ids', store=True)
+
+    @api.depends('member_ids.company_ids')
+    def _compute_member_company_ids(self):
+        for team in self:
+            team.member_company_ids = team.member_ids.mapped('company_ids')
+
     def _get_leads_for_team_lead(self):
         user = self.env.user
         if user.has_group('crm_team_lead.group_crm_team_lead'):
